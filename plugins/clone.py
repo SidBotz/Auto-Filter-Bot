eac78275c7857f04559ed11c32c24c7683ffa2da
SidBotz <sidop148@gmail.com>
2025-01-02 17:21:01 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-# Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
 
 import re
 from Script import script
